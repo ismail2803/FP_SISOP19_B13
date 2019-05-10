985a27872ee902a93bc0be62601442b811f82ef6
Ismail Arifin <ismail.a2803@gmail.com>
2019-05-10 16:25:54 +0700

    Initial commit

---
+++ FP_B13.c
@@ -0,0 +1,185 @@
+#define FUSE_USE_VERSION 28
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+
+static const char *dirpath = "/home/ismail";
+
+void listdir(const char *name)
+{
+    DIR *dir;
+    struct dirent *entry;
+
+    if (!(dir = opendir(name)))
+        return;
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == 4) {
+            char path[1024];
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+                continue;
+            snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);
+
+            listdir(path);
+        } else {
+            char file[1000];
+            char *ext, c = '.';
+            ext = strrchr(entry->d_name, c);
+		    memset(file, '\0', sizeof(file));
+            sprintf(file, "%s/%s", dirpath, entry->d_name);
+            
+            if(ext != NULL){
+                if(strcmp(ext, ".mp3") == 0 && strcmp(name, "/home/ismail") != 0){
+					char fileo[1000];
+					memset(fileo, '\0', sizeof(fileo));
+					sprintf(fileo, "%s/%s", name, entry->d_name);
+
+					pid_t child_id;
+    				child_id = fork();
+					if(child_id == 0){
+						char *argv[4] ={"cp", fileo, file, NULL};
+						execv("/bin/cp", argv);
+					}
+                }    
+            }            
+        }
+    }
+    closedir(dir);
+}
+
+void xmp_destroy(void *private_data){
+	char file[1000];
+	DIR *dir;
+    struct dirent *entry;
+	memset(file, '\0', sizeof(file));
+
+    if (!(dir = opendir(dirpath)))
+        return;
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type != 4) {
+            char *ext, c = '.';
+            ext = strrchr(entry->d_name, c);
+            
+            if(ext != NULL){
+                if(strcmp(ext, ".mp3") == 0){
+					sprintf(file, "%s/%s", dirpath, entry->d_name);
+                    remove(file);
+                }    
+            }            
+        }
+    }
+    closedir(dir);
+}
+
+static int xmp_getattr(const char *path, struct stat *stbuf)
+{
+    int res;
+	char fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	res = lstat(fpath, stbuf);
+
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+		       off_t offset, struct fuse_file_info *fi)
+{
+    char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	int res = 0;
+
+	DIR *dp;
+	struct dirent *de;
+
+	(void) offset;
+	(void) fi;
+
+	dp = opendir(fpath);
+	if (dp == NULL)
+		return -errno;
+
+	while ((de = readdir(dp)) != NULL) {
+		char file[1000];
+		memset(file, '\0', sizeof(file));
+
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+
+		if(de->d_type != 4 || strcmp(de->d_name, ".")==0 || strcmp(de->d_name, "..")==0)
+        {
+			if(de->d_type != 4){	
+				char *ext, c = '.';
+				ext = strrchr(de->d_name, c);
+				
+				if(ext != NULL){
+					if(strcmp(ext, ".mp3") == 0){
+						res = (filler(buf, de->d_name, &st, 0));
+							if(res!=0) break;    
+					}    
+				}
+			}else{
+				res = (filler(buf, de->d_name, &st, 0));
+					if(res!=0) break;
+			}           
+        }
+	}
+
+	closedir(dp);
+	return 0;
+}
+
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
+		    struct fuse_file_info *fi)
+{
+    char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	int res = 0;
+    int fd = 0 ;
+
+	(void) fi;
+	fd = open(fpath, O_RDONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pread(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
+
+static struct fuse_operations xmp_oper = {
+	.destroy	= xmp_destroy,
+	.getattr	= xmp_getattr,
+	.readdir	= xmp_readdir,
+	.read		= xmp_read,
+};
+
+int main(int argc, char *argv[])
+{
+	listdir(dirpath);
+	umask(0);
+	return fuse_main(argc, argv, &xmp_oper, NULL);
+}
---
+++ mp3.c
@@ -0,0 +1,170 @@
+#include <ao/ao.h>
+#include <mpg123.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/time.h>
+
+#define BITS 8
+static const char *dirpath = "/home/ismail/sisop/FP_SISOP19_B13/FP";
+mpg123_handle *mh;
+unsigned char *buffer;
+ao_device *dev;
+int key=0;
+char list[1000][1000];
+int i=0;
+int no=0;
+
+int kbhit(void)
+{
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if(ch != EOF)
+    {
+        ungetc(ch, stdin);
+        return 1;
+    }
+
+    return 0;
+}
+
+void *pilih(void *args){
+    while(1){
+        while(!kbhit());
+        key = getchar() - '0';
+    }
+}
+
+void *putar(void *args){
+    while(1){
+        size_t buffer_size;
+        size_t done;
+        int err;
+
+        int driver;
+
+        ao_sample_format format;
+        int channels, encoding;
+        long rate;
+
+        /* initializations */
+        ao_initialize();
+        driver = ao_default_driver_id();
+        mpg123_init();
+        mh = mpg123_new(NULL, &err);
+        buffer_size = mpg123_outblock(mh);
+        buffer = (unsigned char*) malloc(buffer_size * sizeof(unsigned char));
+
+        /* open the file and get the decoding format */
+        mpg123_open(mh, list[no]);
+        mpg123_getformat(mh, &rate, &channels, &encoding);
+
+        /* set the output format and open the output device */
+        format.bits = mpg123_encsize(encoding) * BITS;
+        format.rate = rate;
+        format.channels = channels;
+        format.byte_format = AO_FMT_NATIVE;
+        format.matrix = 0;
+        dev = ao_open_live(driver, &format, NULL);
+
+        /* decode and play */
+        int flag=1;
+        while(1){
+            printf(" \b\b\b\b\b");
+            if(key == 1){
+                if(mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK){
+                    ao_play(dev, buffer, done);
+                }else{
+                    no++;
+                    no = no % i;
+                    break;
+                }
+            }
+            if(key == 3){
+                key = 1;
+                flag == 0;
+                no++;
+                no = no % i;
+                break;
+            }
+            if(key == 4){
+                key = 1;
+                flag == 0;
+                no--;
+                if(no < 0)
+                    no = i + no;
+                break;
+            }
+        }
+    }
+
+    /* clean up */
+    free(buffer);
+    ao_close(dev);
+    mpg123_close(mh);
+    mpg123_delete(mh);
+    mpg123_exit();
+    ao_shutdown();
+    printf("berhasil");
+}
+
+int main(int argc, char *argv[])
+{
+    // listdir(dirpath);
+    DIR *d;
+
+    struct dirent *dir;
+    d = opendir(dirpath);
+
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            char nama[1000];
+            memset(nama, '\0', sizeof(nama));
+
+            sprintf(nama, "%s/%s", dirpath, dir->d_name);
+            
+            if(strcmp(dir->d_name, ".")!=0 && strcmp(dir->d_name, "..")!=0){
+                strcpy(list[i], nama);
+                i++;
+                printf("[-] %s\n", dir->d_name);
+            }
+        }
+        closedir(d);
+    }
+
+    pthread_t thread1, thread2, thread3, thread4, thread5, thread6, thread7, thread8;
+    int iret1, iret2, iret3, iret4, iret5, iret6, iret7, iret8;
+
+    pthread_create( &thread1, NULL, &pilih, NULL);
+    pthread_create( &thread1, NULL, &putar, NULL);
+
+    while(1){
+
+    }
+
+    return 0;
+}
